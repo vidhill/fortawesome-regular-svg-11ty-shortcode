7bc83e4e77804463db172b8b0cebf9b76da86b3a
David Hill <dh366k@us.att.com>
2021-06-09 18:35:36 +0100

    Add additional test coverage

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,7 @@
-import { fortawesomeFreeRegularShortcode } from './index';
+import {
+    fortawesomeFreeRegularShortcode,
+    fortawesomeFreeRegularPlugin,
+} from './index';
 
 describe("test 'fortawesomeFreeRegularShortcode()'", () => {
     it('should be a defined function', () => {
@@ -11,5 +14,36 @@ describe("test 'fortawesomeFreeRegularShortcode()'", () => {
         expect(result).not.toBeUndefined();
         expect(result).toBeType('string');
         expect(result.length).not.toBe(0);
+    });
+});
+
+describe("test 'fortawesomeFreeRegularPlugin()'", () => {
+    it('should be a defined function', () => {
+        expect(fortawesomeFreeRegularPlugin).not.toBeUndefined();
+        expect(fortawesomeFreeRegularPlugin).toBeType('function');
+    });
+
+    describe('test the plugin', () => {
+        const mockAddShortcode = jest.fn();
+        const eleventyConfig = {
+            addShortcode: mockAddShortcode,
+        };
+
+        beforeEach(() => {
+            mockAddShortcode.mockReset();
+        });
+
+        it("should call 'eleventyConfig.addShortcode()'", () => {
+            fortawesomeFreeRegularPlugin(eleventyConfig);
+            expect(mockAddShortcode).toBeCalledTimes(1);
+        });
+
+        it("should call 'eleventyConfig.addShortcode()' with the expected arguments", () => {
+            fortawesomeFreeRegularPlugin(eleventyConfig);
+            expect(mockAddShortcode).toBeCalledWith(
+                'fortawesomeRegular',
+                fortawesomeFreeRegularShortcode
+            );
+        });
     });
 });
